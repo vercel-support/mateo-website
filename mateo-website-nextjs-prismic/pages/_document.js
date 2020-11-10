3243c5bec84ae0c3b4ea752185d202d7819d0f8e
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2020-11-10 16:23:03 +0100

    Zenhub
    Signed-off-by: Sebastian Frederik Jacobsen <hello@hejsfj.com>

--- mateo-website-nextjs-prismic/pages/_document.js
+++ mateo-website-nextjs-prismic/pages/_document.js
@@ -6,6 +6,8 @@ export default class MyDocument extends Document {
       <Html lang="en">
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-CF3DTMKFR4"></script>
           <script src="scripts/ga.js" async></script>
+          <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=e2787f68-6b11-460d-9abd-9eedd5587844"> </script>
+
         <Head />
         <body>
           <Main />
