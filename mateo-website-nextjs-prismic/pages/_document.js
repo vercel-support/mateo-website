b978716b367e9b355def12244325538c901e7d81
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2021-03-04 11:43:18 +0100

    fix

--- mateo-website-nextjs-prismic/pages/_document.js
+++ mateo-website-nextjs-prismic/pages/_document.js
@@ -1,10 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { hotjar } from 'react-hotjar';
+
 
 export default class MyDocument extends Document {
 
   render() {
-      hotjar.initialize(2279371, 6);
+
     return (
       <Html lang="en">
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-CF3DTMKFR4"></script>
