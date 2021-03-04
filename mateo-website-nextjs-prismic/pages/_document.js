2f57aa630e7ad3320a5fa0ef28c0e5529dd1ac51
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2021-03-04 11:34:48 +0100

    fix hotjar

--- mateo-website-nextjs-prismic/pages/_document.js
+++ mateo-website-nextjs-prismic/pages/_document.js
@@ -1,23 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { hotjar } from 'react-hotjar';
 
 export default class MyDocument extends Document {
+
   render() {
+      hotjar.initialize(2279371, 6);
     return (
       <Html lang="en">
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-CF3DTMKFR4"></script>
           <script src="scripts/ga.js" async></script>
           <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/8830851.js"></script>
-
-          <script>
-              (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:2279371,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          </script>
 
         <Head />
         <body>
